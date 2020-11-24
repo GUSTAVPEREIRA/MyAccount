5d9ec557294e06cd8acc25d618c213e8c7312164
Gustavo Antonio Pereira <gugupereira123@hotmail.com>
2020-11-23 22:18:11 -0300

    Service user functions

--- api/MyAccount/MyAccount/Services/IService/IUserService.cs
+++ api/MyAccount/MyAccount/Services/IService/IUserService.cs
@@ -1,9 +1,13 @@
-﻿using MyAccount.Model;
+﻿using MyAccount.DTO.User;
+using MyAccount.Model;
+using System.Threading.Tasks;
 
 namespace MyAccount.Services.IService
 {
     public interface IUserService
     {
         User GetUserForAuthenticate(string username, string password);
+        Task<User> CreateUser(UserDTO userDTO);
+        Task<UserDTO> GetUserByID(int id);
     }
 }
--- api/MyAccount/MyAccount/Services/Service/UserService.cs
+++ api/MyAccount/MyAccount/Services/Service/UserService.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AutoMapper;
+using MyAccount.DTO.User;
 using MyAccount.Model;
 using MyAccount.Services.IService;
+using MyAccount.Utils.Exceptions;
 
 namespace MyAccount.Services.Service
 {
     public class UserService : IUserService
     {
         private readonly ApplicationContext context;
-
-        public UserService(ApplicationContext context)
+        private readonly IMapper mapper;
+        public UserService(ApplicationContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         public User GetUserForAuthenticate(string username, string password)
@@ -27,6 +33,37 @@ namespace MyAccount.Services.Service
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public async Task<User> CreateUser(UserDTO userDTO)
+        {
+            try
+            {
+                if (context.TbUsers.Where(w => w.Username == userDTO.Username).Any())
+                {
+                    throw new ApiExceptions(HttpStatusCode.BadRequest, "O usuário já está configurado no sistema.");
+                }
+
+                var user = mapper.Map<UserDTO, User>(userDTO);
+
+                await context.TbUsers.AddAsync(user);
+                context.SaveChanges();
+
+                return user;
+            }          
+            catch (ApiExceptions ex)
+            {
+                throw new ApiExceptions(ex.StatusCode, ex.Message, ex);
+            }
+        }
+
+        public async Task<UserDTO> GetUserByID(int id)
+        {
+            var user = await context.TbUsers.FindAsync(id);
+
+            var userDTO = mapper.Map<User, UserDTO>(user);
+
+            return userDTO;
         }
     }
 }
