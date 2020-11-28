cf91c0e13e51b6fad577e3a3d3d658fabc3a469d
unknown <gugupereira123@hotmail.com>
2020-11-28 14:42:47 -0300

    Implement XUnit teste and user simple teste

---
+++ api/MyAccount/MyAccountTests/ContextTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MyAccount;
+using MyAccount.MappingDTO;
+
+namespace MyAccountTests
+{
+    public static class ContextTest
+    {
+        public static ApplicationContext GetContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationContext>().UseInMemoryDatabase("MyAccountTests").Options;
+            var context = new ApplicationContext(options);
+            return context;
+        }
+
+        public static IMapper GetMapping()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new UserMappingDTO());
+            });
+
+            return mockMapper.CreateMapper();
+        }
+    }
+}
---
+++ api/MyAccount/MyAccountTests/User/UserTest.cs
@@ -0,0 +1,50 @@
+using MyAccount;
+using MyAccount.DTO.User;
+using MyAccount.Model;
+using MyAccount.Services.Service;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyAccountTests
+{
+    public class UserTest
+    {
+        private readonly ApplicationContext context;
+        private readonly UserService userService;        
+
+        public UserTest()
+        {
+            context = ContextTest.GetContext();            
+            userService = new UserService(context, ContextTest.GetMapping());
+        }       
+
+        [Fact(DisplayName = "When i enter password, i want it to be encrypted.")]
+        public void IWishPasswordSetEncripted()
+        {
+            string password = "123456";
+
+            User user = new User();
+            user.SetPassword(password);
+
+            Assert.NotEqual(password, user.Password);
+        }
+
+        [Theory(DisplayName = "When i enter a new user, i want created corretly user.")]
+        [InlineData("gpereira", "123456")]
+        public async Task IWishCreateAnUserAsync(string username, string password)
+        {
+            var userDTO = new UserDTO
+            {
+                Username = username,
+                Password = password
+            };
+
+            var user = await userService.CreateUser(userDTO);
+
+            Assert.NotNull(user);
+            Assert.NotEqual(user.CreatedAt, new DateTime());
+            Assert.NotEqual(user.Password, password);            
+        }
+    }
+}
